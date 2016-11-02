b811812cc210d3cd4994d09f1af8b8f0758cb628
omar mohammad <omar mohammad>
2016-11-02 13:46:59 -0400

    home ts modefied

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import{ Http } from '@angular/http'
+import{ NavController } from 'ionic-angular'
+import 'rxjs/add/operator/map'
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  items
+  constructor(public navCtrl: NavController, private http: Http) {
 
-  }
+
+              this.http.get("http://localhost:3000/db").map(res => res.json()).subscribe(data => {
+              this.items = data;
+
+              conole.log("GOT RESULTS");
+              },
+
+
+              error =>
+              {console.log("oops");
+              });
 
 }

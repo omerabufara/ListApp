de64d94e811d4f4a827ec83d4905c80a87ff5601
Abufara <abufara001@gannon.edu>
2016-11-04 12:34:34 -0400

    json working git add .git add .git add .!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import{ Http } from '@angular/http'
-import{ NavController } from 'ionic-angular'
-import 'rxjs/add/operator/map'
+import { Http } from '@angular/http';
+import { NavController } from 'ionic-angular';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -10,18 +10,22 @@ import 'rxjs/add/operator/map'
 export class HomePage {
 
   items
+
   constructor(public navCtrl: NavController, private http: Http) {
+        //this.http.get("http://task.lafrime.net/artists/api.php/artists").map(res => res.json()).subscribe(data => {
+        //this.http.get("https://api.randomuser.me/?results=10").map(res => res.json()).subscribe(data => {
+        this.http.get("http://localhost:3000/db").map(res => res.json()).subscribe(data => {
+        this.items = data;
+        //this.items.push(data);
+        console.log("GOT RESULTS");
+        },
+        error => {
+          console.log("OOPS!");
+        });
+  }
 
-
-              this.http.get("http://localhost:3000/db").map(res => res.json()).subscribe(data => {
-              this.items = data;
-
-              conole.log("GOT RESULTS");
-              },
-
-
-              error =>
-              {console.log("oops");
-              });
+  itemClicked(event, item) {
+    console.log(item.title);
+  }
 
 }
